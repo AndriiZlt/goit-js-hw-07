40f6b3df9fc1df5e34b9caa3f7a029a5ea74b3b6
AndriiZlt <117361082+AndriiZlt@users.noreply.github.com>
2023-03-18 19:31:45 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,14 +2,17 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const cardsMarkup = createCardsMakrup(galleryItems);
-const galleryDivRef = document.querySelector(".gallery");
-galleryDivRef.insertAdjacentHTML("beforeend", cardsMarkup);
+const galleryUlRef = document.querySelector(".gallery");
+
+galleryUlRef.addEventListener("click", onCardClick);
+
+galleryUlRef.insertAdjacentHTML("beforeend", cardsMarkup);
 
 function createCardsMakrup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
+  <a class="gallery__link" href="#">
     <img
       class="gallery__image"
       src="${preview}"
@@ -20,6 +23,14 @@ function createCardsMakrup(galleryItems) {
 </li>`;
     })
     .join("");
+}
+
+function onCardClick(event) {
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
 }
 
 console.log(galleryItems);
