9740d53f079595b2b806995f072b1dda0b7bbc06
AndriiZlt <117361082+AndriiZlt@users.noreply.github.com>
2023-03-18 17:45:42 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const cardsMarkup = createCardsMakrup(galleryItems);
+const galleryDivRef = document.querySelector(".gallery");
+galleryDivRef.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createCardsMakrup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
