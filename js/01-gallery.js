77c53a5812d882828e9c74231f2a5a92c3c19de9
AndriiZlt <117361082+AndriiZlt@users.noreply.github.com>
2023-03-19 13:02:08 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,8 @@ import { galleryItems } from "./gallery-items.js";
 
 const cardsMarkup = createCardsMakrup(galleryItems);
 const galleryUlRef = document.querySelector(".gallery");
-let backDrop = new Object();
+
+let backDrop = new Object(); // Создаю глобальную переменную для LightBox
 
 galleryUlRef.addEventListener("click", onCardClick);
 galleryUlRef.insertAdjacentHTML("beforeend", cardsMarkup);
@@ -26,14 +27,17 @@ function createCardsMakrup(galleryItems) {
 }
 
 function onCardClick(e) {
+  // Инициирую Lightbox в глобальной переменной
   backDrop = basicLightbox.create(`
   <img src="${e.target.dataset.source}" width="800" height="600">
   `);
+  // Не могу передать backdrop как параметр функции onKeyPress потому что потом не работает backDrop.close()
   backDrop.show(() => window.addEventListener("keydown", onKeyPress));
 }
 
 function onKeyPress(e) {
   if (e.key === "Escape") {
+    // Использую глобальную переменную backdrop в другой функции, так как по другому Lightbox не закрывается
     backDrop.close(() => window.removeEventListener("keydown", onKeyPress));
   }
 }
