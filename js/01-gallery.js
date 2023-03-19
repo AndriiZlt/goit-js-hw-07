ae3edbe954017810aa3338eca8d5d61a8a998c9b
AndriiZlt <117361082+AndriiZlt@users.noreply.github.com>
2023-03-19 16:34:04 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-const cardsMarkup = createCardsMakrup(galleryItems);
-const galleryUlRef = document.querySelector(".gallery");
-
-let backDrop = new Object(); // Создаю глобальную переменную для LightBox
-
-galleryUlRef.addEventListener("click", onCardClick);
-galleryUlRef.insertAdjacentHTML("beforeend", cardsMarkup);
-
-function createCardsMakrup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
+const cardsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
   <a class="gallery__link" href="#">
     <img
       class="gallery__image"
@@ -22,23 +12,35 @@ function createCardsMakrup(galleryItems) {
     />
   </a>
 </li>`;
-    })
-    .join("");
-}
+  })
+  .join("");
+
+const galleryUlRef = document.querySelector(".gallery");
+galleryUlRef.insertAdjacentHTML("beforeend", cardsMarkup);
+
+galleryUlRef.addEventListener("click", onCardClick);
 
 function onCardClick(e) {
-  // Инициирую Lightbox в глобальной переменной
-  backDrop = basicLightbox.create(`
+  var instance = basicLightbox.create(
+    `
   <img src="${e.target.dataset.source}" width="800" height="600">
-  `);
-  // Не могу передать backdrop как параметр функции onKeyPress потому что потом не работает backDrop.close()
-  backDrop.show(() => window.addEventListener("keydown", onKeyPress));
-}
+  `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onKeyPress);
+      },
+    }
+  );
+  instance.show();
 
-function onKeyPress(e) {
-  if (e.key === "Escape") {
-    // Использую глобальную переменную backdrop в другой функции, так как по другому Lightbox не закрывается
-    backDrop.close(() => window.removeEventListener("keydown", onKeyPress));
+  function onKeyPress(e) {
+    console.log(e.key);
+    if (e.key === "Escape") {
+      instance.close();
+    }
   }
 }
 
